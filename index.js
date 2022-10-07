3d2c929717a1332dd545b43568847f2e1c224851
jeanpiereche <jeanpiereche@gmail.com>
2022-10-07 13:02:56 -0500

    Change port for deploy

--- index.js
+++ index.js
@@ -77,7 +77,8 @@ app.use(() => {
   console.log('he Entrado aqui')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
